0ef800342ba99dae7250639f8cd3006d38635d37
Gustavo Fernandes <gutof.carvalho@gmail.com>
2026-02-18 16:36:07 -0300

    changing the color of the background and of the status bar

--- app/src/main/java/com/example/f1/MainActivity.kt
+++ app/src/main/java/com/example/f1/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.f1
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
@@ -15,13 +16,16 @@ import com.example.f1.ui.theme.F1Theme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.light(
+                android.graphics.Color.TRANSPARENT,
+                android.graphics.Color.TRANSPARENT
+            )
+        )
         setContent {
             F1Theme {
                 Scaffold(
                     modifier = Modifier.fillMaxSize(),
-
-
                 ) {innerPadding ->
                     Column(
                         modifier = Modifier.padding(innerPadding)
